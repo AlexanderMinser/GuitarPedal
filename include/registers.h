569cd9562525c3c72108e9a31443608a72a6859f
Alexander Minser <adminser@ncsu.edu>
2022-01-15 21:59:13 -0700

    Reorg

--- include/registers.h
+++ include/registers.h
@@ -3,8 +3,10 @@
 
 #if (TARGET_MCU == STM32_F0R8)
     #include "registers_f0r8.h"
-#elif (TARGET_MCU == STM32_L151)
-    #include "registers_l151.h"
+#elif (TARGET_MCU == STM32_L152)
+    #include "registers_l152.h"
+#elif (TARGET_MCU == STM32_F302)
+    #include "registers_f302.h"
 #else
     #error "No processor selected!"
 #endif
