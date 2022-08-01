a297e77c31443214f177ce4c6a83b38dc48aaef6
Alexander Minser <adminser@ncsu.edu>
2022-07-31 19:27:30 -0600

    Forgot to add new file

---
+++ src/led.c
@@ -0,0 +1,13 @@
+
+#include "gpio.h"
+#include "rcc.h"
+#include "registers.h"
+
+void led_init(void) {
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; //enable GPIO port A
+    GPIOA->MODER |= GPIO_MODER5(1);    //enable GPIOA pin 5
+}
+
+void toggle_led(void) {
+    GPIOA->ODR ^= GPIO_ODR5;//(1 << 5);
+}
